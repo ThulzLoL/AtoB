2e8fbdc30e075af21fa2653a9a503e022830b3f7
fmotoike <fmotoike@gmail.com>
2018-05-17 21:59:51 -0300

    Criar menu de login

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,15 +1,35 @@
+let loginBtn = document.querySelector('#login');
+
+function signIn(){
+    let formLogIn = document.querySelector('.log');
+    let menu = document.querySelector("nav");
+    if (formLogIn.style.display === "none"){
+        formLogIn.style.display = "block";
+        formLogIn.style.width = "60%";
+    }
+    else{
+        formLogIn.style.display = "none";
+        formLogIn.style.width = "0";
+    }
+
+}
+
+loginBtn.onclick = signIn;
 let menuBtn = document.querySelector('.menuBtn');
 
 function menuToogle() {
     let menu = document.querySelector("nav");
+    let formLogIn = document.querySelector('.log');
     if (menu.style.display === "none") {
         menu.style.display = "flex";
         menu.style.flexDirection = "column";
-        menu.style.width = "200px";
+        menu.style.width = "40%";
     }
     else {
         menu.style.display = "none";
         menu.style.width = "0px;";
+        formLogIn.style.diplay = "none";
+        formLogIn.style.width = "0";
     }
 }
 
--- src/js/nav.js
+++ src/js/nav.js
@@ -2,14 +2,17 @@ let menuBtn = document.querySelector('.menuBtn');
 
 function menuToogle() {
     let menu = document.querySelector("nav");
+    let formLogIn = document.querySelector('.log');
     if (menu.style.display === "none") {
         menu.style.display = "flex";
         menu.style.flexDirection = "column";
-        menu.style.width = "200px";
+        menu.style.width = "40%";
     }
     else {
         menu.style.display = "none";
         menu.style.width = "0px;";
+        formLogIn.style.diplay = "none";
+        formLogIn.style.width = "0";
     }
 }
 
