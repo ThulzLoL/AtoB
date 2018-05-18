b8efcc68f4399b74dae9a98d39228cb61f5ad58b
Thulz <thulz.lol@gmail.com>
2018-05-18 20:09:40 -0300

    alteracoes sugeridas pelo leo e o comeco do script

--- dist/scripts.js
+++ dist/scripts.js
@@ -19,9 +19,13 @@ function addCart (id){
         
 }
 let comidaImgOne = document.querySelector(".imgOne");
+let nameOne = document.querySelector(".priceOne");
 
 function filterComida(){
     comidaImgOne.src = categorias.food[2].img;
+    nameOne.innerHTML = categorias.food[2].nome;
+
+
 }
 let loginBtn = document.querySelector('#login');
 
--- src/js/filters.js
+++ src/js/filters.js
@@ -1,5 +1,9 @@
 let comidaImgOne = document.querySelector(".imgOne");
+let nameOne = document.querySelector(".priceOne");
 
 function filterComida(){
     comidaImgOne.src = categorias.food[2].img;
+    nameOne.innerHTML = categorias.food[2].nome;
+
+
 }
