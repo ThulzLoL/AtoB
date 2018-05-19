427ebac04b4b899f48d9368a1d5a9b8beba880af
Thulz <thulz.lol@gmail.com>
2018-05-18 22:03:36 -0300

    script de procura dois

--- src/js/searchEngine.js
+++ src/js/searchEngine.js
@@ -2,11 +2,12 @@ const textSearch = document.querySelector(".textSearch");
 
 function lookFor(){
     const nome = textSearch.value;
+    const regex = new RegExp(`${nome}.*`, 'i');
     produtosSection.innerHTML = "";
 
     for (let categoria of Object.values(categorias)){
         for (let produto of categoria) {
-                if (produto.nome.includes(nome)){
+                if (regex.test(produto.nome)){
                     produtosSection.innerHTML += `<div class="product">
             <img src="${produto.img}" alt="">
             <div class="price">
