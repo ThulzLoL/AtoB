5c8aef8a4c31d0f1caefb58ca1e1e21b88ac0463
Thulz <thulz.lol@gmail.com>
2018-05-20 23:24:12 -0300

    adicionado limite ao numero de procuras e ajustes em css

--- dist/scripts.js
+++ dist/scripts.js
@@ -293,15 +293,17 @@ let categorias = {
 
 }
 const textSearch = document.querySelector(".textSearch");
+let counter = 0;
 
 function lookFor(){
     const nome = textSearch.value;
     const regex = new RegExp(`${nome}.*`, 'i');
     produtosSection.innerHTML = "";
-
+    counter = 0;
     for (let categoria of Object.values(categorias)){
         for (let produto of categoria) {
-                if (regex.test(produto.nome)){
+                if (regex.test(produto.nome) && counter <= 3){
+                    counter++;
                     produtosSection.innerHTML += `<div class="product">
             <img src="${produto.img}" alt="">
             <div class="price">
