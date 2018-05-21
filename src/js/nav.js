1faf1e1e1c46b2bf1b0632d18dec40d4950f5beb
fmotoike <fmotoike@gmail.com>
2018-05-21 14:23:27 -0300

    Reajustando menu

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,7 +3,8 @@ let menuBtn = document.querySelector('.menuBtn');
 function menuToogle() {
     let menu = document.querySelector("nav");
     let formLogIn = document.querySelector('.log');
-    if (menu.style.display === "none") {
+   
+    if (menu.style.display !== "flex") {
         menu.style.display = "flex";
         menu.style.flexDirection = "column";
         menu.style.width = "40%";
