d56f381c658def4ecc0c04dc98bb615d2eff1bfa
Leonardo Nagasaki <leonardonagasaki@gmail.com>
2018-05-21 21:38:55 -0300

    adicao imgs produtos e produtos js

--- src/js/produtos.js
+++ src/js/produtos.js
@@ -1,15 +1,15 @@
 let categorias = {
     food: [
         {
-            nome:"Lasanha Bolonhesa - Sadia",
+            nome:"Lasanhas Sabores - Sadia",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/lasanha-sadia.jpg",
             id: 1
         },
         {
             nome:"Nuggets com queijo - Sadia",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/nuggets-queijo-sadia.jpg",
             id: 2
         },
         {
@@ -21,7 +21,7 @@ let categorias = {
         {
             nome:"Pratos Congelados - Marca Fitness",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/congelados-sadia.jpg",
             id: 4
         },
     ],
@@ -30,25 +30,25 @@ let categorias = {
         {
             nome:"Água mineral Garrafa - 600ml - Pack com 6",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/agua-mineral-garrafa-pack6.jpg",
             id: 5
         },
         {
-            nome:"Água mineral Copo - 180ml - Pack com 6",
+            nome:"Água mineral Copo - 180ml - Pack com 3",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/agua-mineral-copo-pack6.jpg",
             id: 6
         },
         {
             nome:"Coca-cola latas - Pack com 6",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/coca-cola-lata-pack6.jpg",
             id: 7
         },
         {
-            nnome:"Sucos Del Valle - Cx - 1l - Pack com 6",
+            nnome:"Sucos Del Valle - Cx - 1l - Pack com 5",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/suco-del-valle-pack5.jpg",
             id: 8
         },
     ],
@@ -57,25 +57,25 @@ let categorias = {
         {
             nome:"Vinho Chileno - 750ml - Cx com 6",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/vinho-chileno.jpg",
             id: 9
         },
         {
             nome:"Champagne - Cx com 6",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/champagne.jpg",
             id: 10
         },
         {
             nome:"Smirnoff Ice - Pack com 6",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/smirnoff-ice-pack6.jpg",
             id: 11
         },
         {
             nome:"Cerveja Bohemia - Pack com 6",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/cerveja-bohemia-pack6.jpg",
             id: 12
         },
     ],
@@ -84,25 +84,25 @@ let categorias = {
         {
             nome:"Trufas Cacau show",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/trufas-cacau-show.jpg",
             id: 13
         },
         {
             nome:"Caixa de bombom - Nestlé",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/caixa-bombom-nestle.jpg",
             id: 14
         },
         {
             nome:"Ruffles média",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/rufles-media.jpg",
             id: 15
         },
         {
             nome:"Amendoim descascado",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/amendoim-descascado-yoki.jpg",
             id: 16
         },
     ],
@@ -111,25 +111,25 @@ let categorias = {
         {
             nome:"Capsulas café - Ristreto - Nespresso - Cx com 16",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/capsulas-nespresso.jpg",
             id: 17
         },
         {
             nome:"Capsulas café - Ristreto - Nespresso",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/capsulas-nespresso.jpg",
             id: 18
         },
         {
             nome:"Capsulas café - Ristreto - Nespresso - Cx com 16",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/capsulas-nespresso.jpg",
             id: 19
         },
         {
             nome:"Capsulas café - Ristreto - Nespresso",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/capsulas-nespresso.jpg",
             id: 20
         },
     ],
@@ -138,25 +138,25 @@ let categorias = {
         {
             nome:"Secador de cabelos - Taiff",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/secador-taiff.jpg",
             id: 21
         },
         {
             nome:"Conj. Toalhas - Buddemeyer",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/toalhas-buddemeyer.jpg",
             id: 22
         },
         {
             nome:"Kit L'Occitane",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/kit-loccitane.jpg",
             id: 23
         },
         {
             nome:"Kit L'Óreal Paris",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/kit-loreal.jpg",
             id: 24
         },
     ],
@@ -165,25 +165,25 @@ let categorias = {
         {
             nome:"Papel higiênico - Pack 24",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/papel-higienico-pack24.jpg",
             id: 25
         },
         {
             nome:"Sabonete líquido",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/sabonete-liquido-natura.jpg",
             id: 26
         },
         {
             nome:"Sabonete - Lux",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/sabonete-lux.jpg",
             id: 27
         },
         {
             nome:"Bom Ar - Spray",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/bom-ar-spray.jpg",
             id: 28
         },
     ],
@@ -192,25 +192,25 @@ let categorias = {
         {
             nome:"Jogo de Cama - Padrão",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/jogo-de-cama.jpg",
             id: 29
         },
         {
             nome:"Jogo de Cama - Hercovitch",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/jogo-de-cama-hercovitch.jpg",
             id: 30
         },
         {
             nome:"Travesseiros",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/travesseiros.jpg",
             id: 31
         },
         {
             nome:"Edredom",
             preco: 15,
-            img: "imgs/lasanha_a_bolonhesa.jpg",
+            img: "imgs/edredom.jpg",
             id: 32
         },
     ],
