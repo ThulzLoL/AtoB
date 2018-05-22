fc18dc609c3fe395a66b2591b2eb16ada177372c
Leonardo Nagasaki <leonardonagasaki@gmail.com>
2018-05-21 22:01:58 -0300

    novo codigo cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -15,6 +15,34 @@ function addCart (id){
     if(produtoProcurado){
         carrinho.push(produtoProcurado);
     }
-    
-        
+
+    let soma = somarPrecos();
 }
+
+function removeFromCart (id) {
+    let i = 0;
+    let indexToRemove = 0;
+
+    for (let produto of carrinho){
+        if (produto.id == id){
+            indexToRemove = i;
+        }
+        
+        i++;
+    }
+
+    carrinho.splice(indexToRemove, 1);
+
+    let soma = somarPrecos();
+}
+
+function somarPrecos () {
+    let soma = 0;
+    
+    for (let produto of carrinho){
+        soma += produto.preco;
+    }
+
+    return soma;
+}
+
